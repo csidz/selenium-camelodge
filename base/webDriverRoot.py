a9bebc1214b01801abc1fe5e96fe100b08b9eefa
csidz <csidhu11@gmail.com>
2018-12-26 15:55:08 -0800

    add cache

---
+++ base/webDriverRoot.py
@@ -0,0 +1,32 @@
+"""
+Creates WebDriver instance based on browser
+"""
+from selenium import webdriver
+
+
+class WebDriverRoot():
+    def __init__(self, browser, base_url):
+        self.browser = browser
+        self.base_url = base_url
+
+    def get_webdriver_instance(self):
+        if self.base_url is None:
+            self.base_url = 'www.camelodge.com'
+
+        if self.browser == 'firefox':
+            driver = webdriver.Firefox(executable_path='geckodriver 2')
+        # Todo Edge browser
+        # Todo Chrome browser
+        # Todo Browserless
+        elif self.browser == 'safari':
+            driver = webdriver.Safari(executable_path='/Applications/Safari.app/Contents/MacOS/Safari')
+        else:
+            driver = webdriver.Firefox(executable_path='geckodriver 2')
+
+        driver.maximize_window()
+        driver.implicitly_wait(3)
+        driver.get(url=self.base_url)
+        return driver
+
+
+
