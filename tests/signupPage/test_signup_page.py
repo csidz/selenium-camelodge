0b7157ce6dcbc51aca253d44b874bda70c2b0e8d
csidz <csidhu11@gmail.com>
2018-12-26 17:34:23 -0800

    integrated onetimesetup fixture

--- tests/signupPage/test_signup_page.py
+++ tests/signupPage/test_signup_page.py
@@ -1,26 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from pages.signupPage.signup_page import SignupPage
 import unittest
-import time
+import pytest
 
 
+@pytest.mark.usefixtures("one_time_setup", "set_up")
 class SignupTests(unittest.TestCase):
-    base_url = "https://camelodge.com/"
-    driver = webdriver.Firefox()
-    driver.maximize_window()
-    driver.implicitly_wait(3)
-    driver.get(base_url)
+
+    @pytest.fixture(autouse=True)
+    def classSetup(self, one_time_setup):
+        self.signup_page = SignupPage(self.driver)
 
     def test_signup_valid_creds(self):
-        signup_page = SignupPage(self.driver)
-        signup_page.signup(email='jack.suze@gmail.com', password='123456', confirm_password='123456')
-
-        result = signup_page.validate_signup_successful()
+        self.signup_page.signup(email='jack.suze@gmail.com', password='123456', confirm_password='123456')
+        result = self.signup_page.validate_signup_successful()
         assert result
-
-        self.driver.close()
-        self.driver.quit()
 
     # test_email_field_is_autofocussed
     # test_invalid_creds
