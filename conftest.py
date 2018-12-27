03c3ef383734b75dc20d6b17921ae21ca1c6a6d4
csidz <csidhu11@gmail.com>
2018-12-26 17:27:10 -0800

    conftest consisting of onetimesetup, cmd params

---
+++ conftest.py
@@ -0,0 +1,47 @@
+import pytest
+from base.webDriverRoot import WebDriverRoot
+
+
+@pytest.fixture()
+def set_up():
+    print("Running method level setup")
+    yield
+    print("Runnning method level teardown")
+
+
+@pytest.fixture(scope="class")
+def one_time_setup(request, browser, base_url):
+    print("Running one time setup")
+    wdr = WebDriverRoot(browser=browser, base_url=base_url)
+    driver = wdr.get_webdriver_instance()
+
+    if request.cls is not None:
+        request.cls.driver = driver
+
+    yield driver
+    driver.quit()
+    print("Running one time tearDown")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="firefox",
+                     help="Input the browser Eg: firefox, chrome, safari, edge or browserless")
+    parser.addoption("--OS_Type", help="Input the operating system Eg: Mac, Windows")
+    parser.addoption("--base_url", default="http://camelodge.com/", help="Input the base url. Eg: http://camelodge.com")
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture(scope="session")
+def os_type(request):
+    return request.config.getoption("--OS_Type")
+
+
+@pytest.fixture(scope="session")
+def base_url(request):
+    return request.config.getoption("--base_url")
+
+
