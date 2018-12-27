d2734c24f524a6a023d0747a775e858df390d133
csidz <csidhu11@gmail.com>
2018-12-26 17:28:52 -0800

    added some helper methods eg: verify text

--- utilities/util.py
+++ utilities/util.py
@@ -9,14 +9,16 @@ Example:
 """
 import time
 import traceback
-import random, string
-import utilities.custom_logger as cl
+import random
+import string
+import utilities.customLogger as cl
 import logging
 import requests
 
+
 class Util(object):
 
-    log = cl.customLogger(logging.INFO)
+    log = cl.custom_logger(logging.INFO)
 
     def sleep(self, sec, info=""):
         """
@@ -29,7 +31,7 @@ class Util(object):
         except InterruptedError:
             traceback.print_stack()
 
-    def getAlphaNumeric(self, length, type='letters'):
+    def get_alphaNumeric(self, length, type='letters'):
         """
         Get random string of characters
 
@@ -51,13 +53,13 @@ class Util(object):
             case = string.ascii_letters
         return alpha_num.join(random.choice(case) for i in range(length))
 
-    def getUniqueName(self, charCount=10):
+    def get_unique_name(self, char_count=10):
         """
         Get a unique name
         """
-        return self.getAlphaNumeric(charCount, 'lower')
+        return self.getAlphaNumeric(char_count, 'lower')
 
-    def getUniqueNameList(self, listSize=5, itemLength=None):
+    def get_unique_name_list(self, list_size=5, item_length=None):
         """
         Get a list of valid email ids
 
@@ -66,12 +68,12 @@ class Util(object):
             itemLength: It should be a list containing number of items equal to the listSize
                         This determines the length of the each item in the list -> [1, 2, 3, 4, 5]
         """
-        nameList = []
-        for i in range(0, listSize):
-            nameList.append(self.getUniqueName(itemLength[i]))
-        return nameList
+        name_list = []
+        for i in range(0, list_size):
+            name_list.append(self.getUniqueName(item_length[i]))
+        return name_list
 
-    def verifyTextContains(self, actualText, expectedText):
+    def verify_text_contains(self, actual_text, expected_text):
         """
         Verify actual text contains expected text string
 
@@ -79,16 +81,16 @@ class Util(object):
             expectedList: Expected Text
             actualList: Actual Text
         """
-        self.log.info("Actual Text From Application Web UI --> :: " + actualText)
-        self.log.info("Expected Text From Application Web UI --> :: " + expectedText)
-        if expectedText.lower() in actualText.lower():
+        self.log.info("Actual Text From Application Web UI --> :: " + actual_text)
+        self.log.info("Expected Text From Application Web UI --> :: " + expected_text)
+        if expected_text.lower() in actual_text.lower():
             self.log.info("### VERIFICATION CONTAINS !!!")
             return True
         else:
             self.log.info("### VERIFICATION DOES NOT CONTAINS !!!")
             return False
 
-    def verifyTextMatch(self, actualText, expectedText):
+    def verify_text_match(self, actual_text, expected_text):
         """
         Verify text match
 
@@ -96,16 +98,16 @@ class Util(object):
             expectedList: Expected Text
             actualList: Actual Text
         """
-        self.log.info("Actual Text From Application Web UI --> :: " + actualText)
-        self.log.info("Expected Text From Application Web UI --> :: " + expectedText)
-        if actualText.lower() == expectedText.lower():
+        self.log.info("Actual Text From Application Web UI --> :: " + actual_text)
+        self.log.info("Expected Text From Application Web UI --> :: " + expected_text)
+        if actual_text.lower() == expected_text.lower():
             self.log.info("### VERIFICATION MATCHED !!!")
             return True
         else:
             self.log.info("### VERIFICATION DOES NOT MATCHED !!!")
             return False
 
-    def verifyListMatch(self, expectedList, actualList):
+    def verify_list_match(self, expected_list, actual_list):
         """
         Verify two list matches
 
@@ -113,9 +115,9 @@ class Util(object):
             expectedList: Expected List
             actualList: Actual List
         """
-        return set(expectedList) == set(actualList)
+        return set(expected_list) == set(actual_list)
 
-    def verifyListContains(self, expectedList, actualList):
+    def verify_list_contains(self, expected_list, actual_list):
         """
         Verify actual list contains elements of expected list
 
@@ -123,22 +125,22 @@ class Util(object):
             expectedList: Expected List
             actualList: Actual List
         """
-        length = len(expectedList)
+        length = len(expected_list)
         for i in range(0, length):
-            if expectedList[i] not in actualList:
+            if expected_list[i] not in actual_list:
                 return False
         else:
             return True
 
-    def getLinkStatus(self, url):
-        linkResponse = requests.get(url=url)
-        return linkResponse.status_code
+    def get_link_status(self, url):
+        link_response = requests.get(url=url)
+        return link_response.status_code
 
-    def verifyLink(self, url):
-        linkStatusCode = self.getLinkStatus(url=url)
-        if linkStatusCode == 200:
-            logging.info(msg=' '.join([url, str(linkStatusCode)]))
+    def verify_link(self, url):
+        link_status_code = self.get_link_status(url=url)
+        if link_status_code == 200:
+            logging.info(msg=' '.join([url, str(link_status_code)]))
             return True
         else:
-            logging.info(msg=' '.join([url, str(linkStatusCode)]))
+            logging.info(msg=' '.join([url, str(link_status_code)]))
             return False
