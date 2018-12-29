5da3bd60d11818f4f4cc3a15e040c52fc8751dc6
csidz <csidhu11@gmail.com>
2018-12-28 23:45:09 -0800

    add firefox headless support

--- base/webDriverRoot.py
+++ base/webDriverRoot.py
@@ -2,7 +2,8 @@
 Creates WebDriver instance based on browser
 """
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as c_options
+from selenium.webdriver.firefox.options import Options as ff_options
 import os
 
 
@@ -18,20 +19,24 @@ class WebDriverRoot():
         if self.browser == 'firefox':
             driver = webdriver.Firefox()
         # Todo Edge browser
-        # Todo Browserless
+
         elif self.browser == 'safari':
             driver = webdriver.Safari(executable_path='/Applications/Safari.app/Contents/MacOS/Safari')
-        elif self.browser == 'chrome_headless':
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--window_size=2560X1600")
-            print(os.getcwd() + "/chromedriver")
-            chrome_driver = os.getcwd() + "/chromedriver"
-            driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
         elif self.browser == 'chrome':
-            print(os.getcwd() + "/chromedriver")
             chrome_driver = os.getcwd() + "/chromedriver"
             driver = webdriver.Chrome(executable_path=chrome_driver)
+        elif self.browser == 'firefox_headless':
+            firefox_options = ff_options()
+            firefox_options.headless = True
+            firefox_options.add_argument("--window_size=2560X1600")
+            gecko_driver = os.getcwd() + "/geckodriver"
+            driver = webdriver.Firefox(options=firefox_options, executable_path=gecko_driver)
+        elif self.browser == 'chrome_headless':
+            chrome_options = c_options()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--window_size=2560X1600")
+            chrome_driver = os.getcwd() + "/chromedriver"
+            driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
         else:
             driver = webdriver.Firefox()
 
