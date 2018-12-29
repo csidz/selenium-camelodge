10bd4eddf0b1ae9eefd9c8b464351b9337e36c14
csidz <csidhu11@gmail.com>
2018-12-28 22:55:52 -0800

    add chrome and chrome_headless support

--- base/webDriverRoot.py
+++ base/webDriverRoot.py
@@ -2,6 +2,8 @@
 Creates WebDriver instance based on browser
 """
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
 
 class WebDriverRoot():
@@ -16,10 +18,20 @@ class WebDriverRoot():
         if self.browser == 'firefox':
             driver = webdriver.Firefox()
         # Todo Edge browser
-        # Todo Chrome browser
         # Todo Browserless
         elif self.browser == 'safari':
             driver = webdriver.Safari(executable_path='/Applications/Safari.app/Contents/MacOS/Safari')
+        elif self.browser == 'chrome_headless':
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--window_size=2560X1600")
+            print(os.getcwd() + "/chromedriver")
+            chrome_driver = os.getcwd() + "/chromedriver"
+            driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
+        elif self.browser == 'chrome':
+            print(os.getcwd() + "/chromedriver")
+            chrome_driver = os.getcwd() + "/chromedriver"
+            driver = webdriver.Chrome(executable_path=chrome_driver)
         else:
             driver = webdriver.Firefox()
 
