a26c3151b2680fc7adb0d1da9dbf3fdd3e7f1c30
csidz <csidhu11@gmail.com>
2019-01-07 16:56:37 -0800

    add test_email_field_is_autofocused, test_signup_different_password_confirm_password, validate signuo successful message, MyAccount link

--- tests/test_signupPage/test_signup_page.py
+++ tests/test_signupPage/test_signup_page.py
@@ -1,22 +1,38 @@
 from pages.signupPage.signup_page import SignupPage
-import unittest
+from pages.homePage.home_page import HomePage
+from pages.usersPage.users_page import UsersPage
 import pytest
-import pytest_assume
 
 
 @pytest.mark.usefixtures("one_time_setup", "set_up")
-class TestSignupTests(unittest.TestCase):
+class TestSignupTests():
     @pytest.fixture(autouse=True)
     def classSetup(self, one_time_setup):
         self.signup_page = SignupPage(self.driver)
+        self.home_page = HomePage(self.driver)
+        self.users_page = UsersPage(self.driver)
 
+    def test_email_field_is_autofocused(self):
+        self.signup_page.click_signup_link()
+        autofocus_on_email = self.signup_page.validate_autofocus_on_email()
+        assert autofocus_on_email
+
+    def test_signup_different_password_confirm_password(self):
+        self.signup_page.signup(email='jack.suze@gmail.com', password='12345689', confirm_password='1234567')
+        users_page_title = self.users_page.validate_users_page_title()
+        users_page_text = self.users_page.validate_users_page_text()
+        pytest.assume(users_page_title)
+        pytest.assume(users_page_text)
+
+# Todo: User can signup even when signup_successful_message is not displayed. Based on signup_successful_message
+#       we cannot fail the signup case. We should separate the validation of signup_successful_message and assign
+#       low priority
     def test_signup_valid_creds(self):
         self.signup_page.signup(email='jack.suze@gmail.com', password='123456', confirm_password='123456')
-        result = self.signup_page.validate_signup_successful()
-        pytest_assume(result)
-        #assert result
+        signup_successful_message = self.signup_page.validate_signup_successful_message()
+        my_account_link_text = self.home_page.validate_edit_account_link_present()
+        pytest.assume(signup_successful_message)
+        pytest.assume(my_account_link_text)
 
-    # test_email_field_is_autofocussed
     # test_invalid_creds
-    # test_signup_different_password_confirm_password
     # test_signup_existing_user
