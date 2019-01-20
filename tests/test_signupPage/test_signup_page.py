fc9e57a22482e3239f194593fa745b0bac0ea91f
csidz <csidhu11@gmail.com>
2019-01-19 19:58:44 -0800

    signup names and passwords from environment variables

--- tests/test_signupPage/test_signup_page.py
+++ tests/test_signupPage/test_signup_page.py
@@ -18,7 +18,7 @@ class TestSignupTests():
         assert autofocus_on_email
 
     def test_signup_different_password_confirm_password(self):
-        self.signup_page.signup(email='jack.suze@gmail.com', password='12345689', confirm_password='1234567')
+        self.signup_page.signup(email="${env.SIGNUP-EMAIL}", password="${env.SIGNUP-PASSWORD}", confirm_password="${env.SIGNUP-CONFIRM-PASSWORD-DIFFERENT}")
         users_page_title = self.users_page.validate_users_page_title()
         users_page_text = self.users_page.validate_users_page_text()
         pytest.assume(users_page_title)
@@ -28,7 +28,9 @@ class TestSignupTests():
 #       we cannot fail the signup case. We should separate the validation of signup_successful_message and assign
 #       low priority
     def test_signup_valid_creds(self):
-        self.signup_page.signup(email='jack.suze@gmail.com', password='123456', confirm_password='123456')
+        self.signup_page.clear_cookies()
+        self.signup_page.signup(email="${env.SIGNUP-EMAIL}", password="${env.SIGNUP-PASSWORD}",
+                                confirm_password="${env.SIGNUP-CONFIRM-PASSWORD-SAME}")
         signup_successful_message = self.signup_page.validate_signup_successful_message()
         my_account_link_text = self.home_page.validate_edit_account_link_present()
         pytest.assume(signup_successful_message)
